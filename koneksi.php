0aed00f42ce27112d7e4793229635dd0b4104f19
Asri Kinanti Febriany <72858439+febongz@users.noreply.github.com>
2021-05-10 18:14:55 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php  
+// Konfigurasi database
+$host = "localhost"; // Nama host
+$username = "root"; // Nama user
+$password = ""; 
+$database = "db_siswa"; // Nama database
+// Perintah php untuk akses ke database
+$koneksi = mysqli_connect($host,$username,$password,$database); // Variabel koneksi untuk menyambungkan ke database
+?>
